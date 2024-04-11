071039ad736364b60a5f376ff306b87cf1793035
João Paulo Alves de Moura Câmara Bezerra <52804667+Jpamcb@users.noreply.github.com>
2024-04-11 19:30:18 -0300

    Create GuitarPlayer.java

---
+++ GuitarPlayer.java
@@ -0,0 +1,11 @@
+package br.ufrn.imd;
+
+public class GuitarPlayer extends Musician{
+	int howManyYears;
+	boolean playEletronicGuitar;
+	boolean playAcousticGuitar;
+	
+	GuitarPlayer();
+	
+	public void playGuitar();
+}
